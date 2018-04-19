0ec0c19de07f4a710c8ad90a190c14cd5cac4bac
iwest <bokthaku@gmail.com>
2018-04-18 22:39:13 -0400

    Initial work

---
+++ js/core.js
@@ -0,0 +1,57 @@
+(function() {
+    $(window).scroll(function() {
+        var $header = $('nav.top-bar, div.top-bar-spacer');
+        if ($(document).scrollTop() > 0) {
+            $header.addClass('shrink');
+        } else {
+            $header.removeClass('shrink');
+        }
+    });
+
+    $('#submit').click(function() {
+        var $name = $('#contactName');
+        var name = $name.val();
+        var $message = $('#message');
+        var body = $message.val();
+
+        var error = false;
+
+        if (!name) {
+            $name.addClass('error');
+            error = true;
+        } else {
+            $name.removeClass('error');
+        }
+
+        if (!body) {
+            $message.addClass('error');
+            error = true;
+        } else {
+            $message.removeClass('error');
+        }
+
+        if (error) {
+            return;
+        }
+
+        $.ajax({
+            url: "https://node.claire-west.ca/serpens/contact",
+            method: "POST",
+            data: JSON.stringify({
+                value1: name,
+                value2: body
+            })
+        }).done(function() {
+            $('.modal .dialog h4').text('Message Sent');
+            $('.modal').show();
+        }).fail(function() {
+            $('.modal .dialog h4').text('Error');
+            $('.modal .dialog p').text('Unable to deliver message.')
+            $('.modal').show();
+        });
+    });
+
+    $('.modal .dialog button').click(function() {
+        $('.modal').hide();
+    });
+})();
